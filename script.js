0ba5b58fb31d07b08f62f2582d2426c19b3b9445
strawberryaish <trismorgenstern@gmail.com>
2024-04-19 23:28:03 +0800

    add media queries

--- script.js
+++ script.js
@@ -11,16 +11,19 @@ const togglingAnswer = iterate => {
     let iterateIcon = document.getElementById(`icon${iterate + 1}`);
 
     if (iterateAnswer) {
+    
        iterateAnswer.classList.toggle('hidden');
     
        if (!iterateAnswer.classList.contains('hidden'))
        {
-        
+       //MINUS
+       iterateIcon.style.transition = 'all 0.3s linear';
        iterateIcon.setAttribute('d', 'M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z');
        }
 
        else {
-
+        //PLUS
+       iterateIcon.style.transition = 'all 0.1s linear';
        iterateIcon.setAttribute('d', 'M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z');
 
        }
@@ -34,11 +37,6 @@ const togglingAnswer = iterate => {
 
     btn.addEventListener('click', e => { e.stopPropagation(); togglingAnswer(index); })})
 });
-
-
-
-
-
 
 
 
